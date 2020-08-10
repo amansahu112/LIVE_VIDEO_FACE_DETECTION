004ae8614813d43a221d9e51e0cb13676ac64bfd
amansahu112 <47480917+amansahu112@users.noreply.github.com>
2020-08-10 15:30:42 +0530

    Add files via upload

---
+++ camera.py
@@ -0,0 +1,18 @@
+import cv2
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+ds_factor=0.6
+class VideoCamera(object):
+    def __init__(self):
+        self.video = cv2.VideoCapture(0)
+
+    def __del__(self):
+        self.video.release()
+
+    def get_frame(self):
+        ret, frame = self.video.read()
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        ret, jpeg = cv2.imencode('.jpg', frame)
+        return jpeg.tobytes()
